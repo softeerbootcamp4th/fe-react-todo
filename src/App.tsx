e612f743b1dac1cb0d3301ec15f2dd595f9b1fae
jhj2713 <juhyojeong58@gmail.com>
2024-07-16 14:34:38 +0900

    feat: input 제출 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Input from "./components/Input";
 
 function App() {
     return (
         <div className="flex w-screen h-screen justify-center items-center">
-            <div className="w-[500px] h-[600px]">
+            <div className="w-[500px] h-[600px] flex flex-col gap-10">
                 <Header />
+                <Input />
             </div>
         </div>
     );
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from "react";
+import { TodoAPI } from "../../apis/todoAPI";
 
 function Input() {
     const [inputValue, setInputValue] = useState("");
@@ -7,13 +8,27 @@ function Input() {
         setInputValue(e.target.value);
     };
 
-    const handleSubmit = () => {};
+    const handleSubmit = () => {
+        TodoAPI.post({ description: inputValue });
+    };
 
     return (
-        <>
-            <input value={inputValue} onChange={handleInputChange} />
-            <button onClick={handleSubmit}></button>
-        </>
+        <div className="relative">
+            <input
+                className="block w-full p-4 pr-20 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+                placeholder="할 일을 입력하세요"
+                value={inputValue}
+                onChange={handleInputChange}
+                required
+            />
+            <button
+                type="submit"
+                className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+                onClick={handleSubmit}
+            >
+                등록
+            </button>
+        </div>
     );
 }
 
