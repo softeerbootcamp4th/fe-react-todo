6eed4dd05fd603f688ccca0e7169d94ad42c9a6f
darkdulgi <daniel99285@gmail.com>
2024-07-16 15:40:03 +0900

    [feat] json-server 활용한 서버 통신과 todo list 삽입, 삭제 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,36 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getData, pushData, popData } from "/utils/db";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [formString, setFormString] = useState("");
+
+  useEffect(() => {
+    getData().then((data) => {
+      setTodoList(data);
+    });
+  }, [])
 
   function onFormChange(e) {
     const string = e.target.value;
     setFormString(string);
   };
 
-  function onButtonClick(e) {
+  function onClickPush(e) {
     e.preventDefault();
     if (formString) {
-      setTodoList([...todoList, formString]);
+      pushData({ title: formString }).then((newId) => {
+        setTodoList([...todoList, { id: newId, title: formString }]);
+      });
     }
     setFormString("");
   };
 
-  console.log(todoList);
+  function onClickPop(id, e) {
+    e.preventDefault();
+    setTodoList(todoList.filter((todo) => todo.id !== id));
+    popData(id);
+  }
 
   return (
     <div className="flex justify-center">
@@ -26,12 +39,13 @@ function App() {
 
         <form>
           <input value={formString} onChange={onFormChange} type="text" className="border border-black" />
-          <button onClick={onButtonClick}>등록</button>
+          <button onClick={onClickPush} className="bg-green-700 text-white">등록</button>
         </form>
 
-        {todoList.map((todo, idx) => (
-          <div key={idx}>
-            {todo}
+        {todoList.map((todo) => (
+          <div key={todo.id}>
+            <span>{todo.title}</span>
+            <button onClick={(e) => { onClickPop(todo.id, e); }} className="bg-blue-700 text-white">삭제</button>
           </div>
         ))}
       </div>
---
+++ utils/db.jsx
@@ -0,0 +1,21 @@
+export async function getData() {
+  const data = await fetch("http://localhost:3000/todo");
+  const dataList = await data.json();
+  return dataList;
+}
+
+export async function pushData(newData) {
+  const promise = await fetch("http://localhost:3000/todo", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(newData)
+  });
+  const data = await promise.json();
+  return data.id;
+}
+
+export async function popData(id) {
+  fetch(`http://localhost:3000/todo/${id}`, {
+    method: "DELETE",
+  })
+}
