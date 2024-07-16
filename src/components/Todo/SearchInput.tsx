af5c282a71fc1b660b1c8d3ad4d84b4402b44ec3
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-17 00:13:32 +0900

    fix. import api from apis folder

--- src/components/Todo/SearchInput.tsx
+++ src/components/Todo/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { useState, ChangeEvent } from 'react';
-import { BASE_URL } from '../../apis/fetch';
+import { handleSubmit } from '../../apis/fetch';
 
 function SearchInput() {
 
@@ -10,31 +10,10 @@ function SearchInput() {
     SetNewItem(e.target.value);
   }
 
-  const handleSubmit = async () => {
-    //공백 방지
-    if (!newItem.trim()) {
-      alert('할일을 입력하세요.');
-      return;
-    }
-
-    try{
-      const response = await fetch( `${BASE_URL}/todos`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ text: newItem, completed: false })
-      });
-
-      const responseData = await response.json();
-
-      console.log('추가 성공', responseData);
-      SetNewItem('');
-    } catch(error){
-      console.error('추가 실패', error);
-    }
+  const handleNewItem = () => {
+    handleSubmit(newItem);
+    SetNewItem('');  
   }
-
   return (
     <div className="justify-center items-center border-l-2 ml-2 w-[300px] flex-row p-2">
       <input type="text" 
@@ -43,7 +22,7 @@ function SearchInput() {
         onChange={handleInputChange}
         placeholder="할일을 입력하세요"
       />
-      <button className=" bg-green-600 text-white ml-[10px]" onClick={handleSubmit}>등록</button>
+      <button className=" bg-green-600 text-white ml-[10px]" onClick={handleNewItem}>등록</button>
     </div>
   )
 }
