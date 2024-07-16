b03a407d4fb88a345ad2c8c9ac5e1d15f649f6ec
jhj2713 <juhyojeong58@gmail.com>
2024-07-16 17:45:49 +0900

    feat: 버튼 및 input border 색상 변경

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -24,7 +24,7 @@ function Input() {
     return (
         <form className="relative" onSubmit={handleSubmit}>
             <input
-                className="block w-full p-4 pr-20 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 text-xl"
+                className="block w-full p-4 pr-20 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:border-indigo-400 text-xl"
                 placeholder="할 일을 입력하세요"
                 value={inputValue}
                 onChange={handleInputChange}
@@ -32,7 +32,7 @@ function Input() {
             />
             <button
                 type="submit"
-                className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-4 py-2 text-xl"
+                className="text-white absolute end-2.5 bottom-2.5 bg-indigo-400 hover:bg-indigo-500 focus:ring-2 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg px-4 py-2 text-xl"
             >
                 등록
             </button>
