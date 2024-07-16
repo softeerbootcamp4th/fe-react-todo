8d90551fec8d8224ce366beb1851f87369a13783
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-16 18:42:45 +0900

    feat. todolist layout

---
+++ src/components/Todo/Todo.tsx
@@ -0,0 +1,15 @@
+import SearchInput from './SearchInput';
+import TodoList from './TodoList';
+
+function Todo() {
+  return (
+    <div className='flex justify-center items-center'>
+      <div className="flex flex-col items-center p-2 w-[500px]">
+        <SearchInput />
+        <TodoList />
+      </div>
+    </div>
+  )
+}
+
+export default Todo
