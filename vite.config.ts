58da1cfcd9cf6b82ee869697df38114ac6c02876
bomin <nimod1234@g.skku.edu>
2024-07-16 10:56:42 +0900

    Feature: context api, storybook, tailwind css, vitest 설정 (#1)
    * chore: alias import path 제거
    
    * refactor: use export default function
    
    * style: add blank lines between code for readability
    
    * chore: set tailwindcss
    
    * chore: set storybook
    
    * chore: cache 저장 위치 변경
    
    * chore: set vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig, InlineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  cacheDir: "./.vite",
+  cacheDir: "./.yarn/.vite",
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
   server: { port: 3000 },
-});
+} as VitestConfigExport);
