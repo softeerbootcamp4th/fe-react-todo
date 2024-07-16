5d96965785874d8e7bbe84bd980f5d6b357e6a71
jhj2713 <juhyojeong58@gmail.com>
2024-07-16 11:40:34 +0900

    chore: json server 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react";
+
 function App() {
+    useEffect(() => {
+        getData();
+    }, []);
+
+    const getData = async () => {
+        await fetch("http://localhost:3000/todo")
+            .then(data => {
+                return data.json();
+            })
+            .then(data => {
+                console.log(data);
+            });
+    };
+
     return <div className="text-red-100">hello</div>;
 }
 
