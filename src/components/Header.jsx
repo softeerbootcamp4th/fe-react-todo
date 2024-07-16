b70168af2a21c40507001636092fd707a9085a4d
subsub-e <ab13696802@gmail.come>
2024-07-16 17:48:10 +0900

    [layout] header

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/header.module.css"
+
+function Header() {
+    return (
+        <header className={styles.header}>My Todo App</header>
+    );
+}
+
+export default Header;
