71eccf5b75babff4b79d1739ac92f3cff995f398
darkdulgi <daniel99285@gmail.com>
2024-07-16 12:15:45 +0900

    [feat] 기초 상태와 와이어프레임 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
+import { useState } from "react"
+
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [formString, setFormString] = useState("");
+
+  function onFormChange(e) {
+    const string = e.target.value;
+    setFormString(string);
+  };
+
+  function onButtonClick(e) {
+    e.preventDefault();
+    if (formString) {
+      setTodoList([...todoList, formString]);
+    }
+    setFormString("");
+  };
+
+  console.log(todoList);
 
   return (
-    <div className="">
-      Hello
+    <div className="flex justify-center">
+      <div className="max-w-3xl border border-gray-500">
+        <span>My Todo App</span>
+
+        <form>
+          <input value={formString} onChange={onFormChange} type="text" className="border border-black" />
+          <button onClick={onButtonClick}>등록</button>
+        </form>
+
+        {todoList.map((todo, idx) => (
+          <div key={idx}>
+            {todo}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
