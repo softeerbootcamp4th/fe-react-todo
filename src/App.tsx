f49d425eed2fe75c75022bec361b35bab0194a35
pccommen <pccommen@gmail.com>
2024-07-17 18:36:48 +0900

    fix: 기존 방식에서 props를 내려주는 로직으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header.tsx'
 import LogList from './LogList.tsx'
-import TextField from "./TextField.tsx"
+import TextField from './TextField.tsx'
 import ToDoList from './ToDoList.tsx';
+import "./index.css"
 
 const App: React.FC = () => {
+    const [toDo, setToDo] = useState<string>("a");
+    const [toDos, setToDos] = useState<string[]>([]);
+
+    const addToDo = (todo: string): undefined => {
+
+        setToDos(currentArray => [todo, ...currentArray]);
+    };
+
+    const changCurrentToDo = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log("app " + event.target.value)
+        setToDo(event.target.value)
+    }
+
     return (
         <>
             <div className="min-h-screen flex">
                 <div className="flex flex-col items-center w-full">
                     <Header />
-                    <TextField />
-                    <ToDoList />
+                    <TextField toDo={toDo} setToDo={changCurrentToDo} addToDo={addToDo} />
+                    <ToDoList toDos={toDos} />
                 </div>
                 <div className="absolute right-10 top-10 z-10">
                     <div className=''>
