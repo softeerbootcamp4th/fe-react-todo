34111c9b1bc937012e678e77df47d7d0b7a0bc91
pccommen <pccommen@gmail.com>
2024-07-17 16:56:26 +0900

    feature: Header 구현

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,8 +1,9 @@
-import './App.css';
+import React from 'react';
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
-      <h1 className="px-20"> My Todo App</h1>
+    <h1 className="px-20"> My Todo App</h1>
+    //<span className="font-bold text-6xl mt-16"> My Todo List</span> a8df159 (feature: Header 구현)
   )
 }
 
