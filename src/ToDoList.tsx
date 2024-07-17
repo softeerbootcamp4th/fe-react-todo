fe906b320722c66e3a8c6901430904211f2bdde6
pccommen <pccommen@gmail.com>
2024-07-17 18:38:28 +0900

    fix: props를 받아서 로직을 수행하는 방식으로 변경

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,15 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ToDoListElement from "./ToDoListElement";
 
+interface ToDoListProps {
+    toDos: string[];
+}
 
-const ToDoList: React.FC = () => {
-    const [toDos, setToDos] = useState(["a", "b", "c", "d", "e", "a", "b", "c", "d", "e"])
 
-    useEffect(() => {
-        // const reponse = fetch().....
-        // setToDos(response)
-    }, [])
-
+const ToDoList: React.FC<ToDoListProps> = ({ toDos }: ToDoListProps) => {
     const content = (
         <div className="flex flex-col items-center w-full overflow-y-scroll h-1/3 mt-6">
             {toDos.map((todo, index) => (<ToDoListElement key={index} todo={todo} />))}
