bfdfc55042e7ed091abdb5818d54a8f02018fa91
bluejoyq <bluejoy32@gmail.com>
2024-07-17 14:37:10 +0900

    refactor: db 오브젝트에서 나머지 기능 접근하도록 응집

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,22 +1,24 @@
 import { Todo } from "../../src/models/Todo";
 
-export const insertTodo = async (todo: Todo) => {
-  const currentTodos = await readTodos();
-  localStorage.setItem("todos", JSON.stringify([...currentTodos, todo]));
-};
+export const db = {
+  insertTodo: async (todo: Todo) => {
+    const currentTodos = await db.readTodos();
+    localStorage.setItem("todos", JSON.stringify([...currentTodos, todo]));
+  },
 
-export const readTodos = async (): Promise<Array<Todo>> => {
-  return JSON.parse(localStorage.getItem("todos") || "[]");
-};
+  readTodos: async (): Promise<Array<Todo>> => {
+    return JSON.parse(localStorage.getItem("todos") || "[]");
+  },
 
-export const updateTodo = async (todo: Todo) => {
-  const currentTodos = await readTodos();
-  const updatedTodos = currentTodos.map((t) => (t.id === todo.id ? todo : t));
-  localStorage.setItem("todos", JSON.stringify(updatedTodos));
-};
+  updateTodo: async (todo: Todo) => {
+    const currentTodos = await db.readTodos();
+    const updatedTodos = currentTodos.map((t) => (t.id === todo.id ? todo : t));
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  },
 
-export const removeTodo = async (id: number) => {
-  const currentTodos = await readTodos();
-  const updatedTodos = currentTodos.filter((t) => t.id !== id);
-  localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  removeTodo: async (id: number) => {
+    const currentTodos = await db.readTodos();
+    const updatedTodos = currentTodos.filter((t) => t.id !== id);
+    localStorage.setItem("todos", JSON.stringify(updatedTodos));
+  },
 };
