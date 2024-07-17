3cda8ec31d5463fd8f3ef1c5733092fe55dfd08b
jhj2713 <juhyojeong58@gmail.com>
2024-07-17 14:22:30 +0900

    feat: 수정 로직 구현

--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { TodoItemProps } from "../../types/todo";
 import { TodoAPI } from "../../apis/todoAPI";
 import { useTodoList } from "../../hooks/useTodoList";
@@ -6,12 +6,27 @@ import { useTodoList } from "../../hooks/useTodoList";
 function TodoItem({ todo }: TodoItemProps) {
     const { getTodoList } = useTodoList();
 
-    const [isChecked, setIsChecked] = useState(todo.isChecked);
-    const [inputValue, setInputValue] = useState(todo.description);
+    const [isChecked, setIsChecked] = useState<boolean>(todo.isChecked);
+    const [inputValue, setInputValue] = useState<string>(todo.description);
+
+    const [isEditing, setIsEditing] = useState<boolean>(false);
+    const timeoutRef = useRef<number | null>(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (isEditing && inputRef.current) {
+            inputRef.current.focus();
+        }
+    }, [isEditing]);
 
     const handleCheckChange = async (e: ChangeEvent<HTMLInputElement>) => {
-        setIsChecked(e.target.checked);
-        await TodoAPI.patch({ ...todo, isChecked: e.target.checked });
+        try {
+            const changedCheck = e.target.checked;
+            await TodoAPI.patch({ ...todo, isChecked: changedCheck });
+            setIsChecked(changedCheck);
+        } catch (error) {
+            console.error("Error:", error);
+        }
     };
 
     const handleDelete = async () => {
@@ -23,12 +38,31 @@ function TodoItem({ todo }: TodoItemProps) {
         }
     };
 
-    const handleDescriptionClick = () => {
-        // TODO: 시간 재서 수정 input으로 변경
+    const handleEdit = async () => {
+        try {
+            await TodoAPI.patch({ ...todo, description: inputValue });
+            setIsEditing(false);
+            getTodoList();
+        } catch (error) {
+            console.error("Error:", error);
+        }
     };
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         setInputValue(e.target.value);
+    };
+
+    const handleMouseDown = () => {
+        timeoutRef.current = setTimeout(() => {
+            setIsEditing(true);
+        }, 2000);
+    };
+
+    const handleMouseUp = () => {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+            timeoutRef.current = null;
+        }
     };
 
     return (
@@ -43,27 +77,43 @@ function TodoItem({ todo }: TodoItemProps) {
             />
 
             <div className="grow flex justify-between items-center">
-                <label
-                    htmlFor={`${todo.id}-checkbox`}
-                    className={`${isChecked ? "line-through text-gray-400" : ""}`}
-                    onClick={handleDescriptionClick}
-                >
-                    {todo.description}
-                </label>
-                {/* <input
-                    className="block w-80 text-gray-900 border-b focus:border-indigo-400 text-xl"
-                    placeholder="할 일을 입력하세요"
-                    value={inputValue}
-                    onChange={handleInputChange}
-                    required
-                /> */}
-                <button
-                    type="submit"
-                    className="text-white bg-rose-400 hover:bg-rose-500 focus:ring-2 focus:outline-none focus:ring-rose-300 font-medium rounded-lg px-4 py-2 text-xl"
-                    onClick={handleDelete}
-                >
-                    삭제
-                </button>
+                {isEditing ? (
+                    <>
+                        <input
+                            ref={inputRef}
+                            className="block w-80 text-gray-900 border-b focus:border-indigo-400 text-xl"
+                            placeholder="할 일을 입력하세요"
+                            value={inputValue}
+                            onChange={handleInputChange}
+                            required
+                        />
+                        <button
+                            type="button"
+                            className="text-white bg-indigo-400 hover:bg-indigo-500 focus:ring-2 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg px-4 py-2 text-xl"
+                            onClick={handleEdit}
+                        >
+                            수정
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <label
+                            htmlFor={`${todo.id}-checkbox`}
+                            className={`${isChecked ? "line-through text-gray-400" : ""}`}
+                            onMouseDown={handleMouseDown}
+                            onMouseUp={handleMouseUp}
+                        >
+                            {todo.description}
+                        </label>
+                        <button
+                            type="button"
+                            className="text-white bg-rose-400 hover:bg-rose-500 focus:ring-2 focus:outline-none focus:ring-rose-300 font-medium rounded-lg px-4 py-2 text-xl"
+                            onClick={handleDelete}
+                        >
+                            삭제
+                        </button>
+                    </>
+                )}
             </div>
         </li>
     );
