d95cfc4c13d38215304a8ccfb8b73bdfe4002ac1
bluejoyq <bluejoy32@gmail.com>
2024-07-17 14:10:17 +0900

    fix: longpress에서 state로 변경

--- src/hooks/useLongPress.ts
+++ src/hooks/useLongPress.ts
@@ -1,31 +1,37 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export const useLongPress = (onLongPress: () => void, ms = 300) => {
-  let timer: number;
+  const [currentNode, setCurrentNode] = useState<HTMLElement | null>(null);
+  const timerRef = useRef<number | null>(null);
+  const start = useCallback(() => {
+    timerRef.current = setTimeout(onLongPress, ms);
+  }, [ms, onLongPress]);
 
-  const ref = useRef<HTMLDivElement | null>(null);
-  const start = () => {
-    timer = setTimeout(onLongPress, ms);
-  };
-
-  const stop = () => {
-    clearTimeout(timer);
-  };
-  useEffect(() => {
-    const el = ref.current;
-    if (!el) {
+  const stop = useCallback(() => {
+    if (timerRef.current == null) {
       return;
     }
-    el.addEventListener("mousedown", start);
-    el.addEventListener("touchstart", start);
-    el.addEventListener("mouseup", stop);
-    el.addEventListener("touchend", stop);
+    clearTimeout(timerRef.current);
+    timerRef.current = null;
+  }, []);
+  useEffect(() => {
+    if (!currentNode) return;
+    currentNode.addEventListener("mousedown", start);
+    currentNode.addEventListener("touchstart", start);
+    currentNode.addEventListener("mouseup", stop);
+    currentNode.addEventListener("touchend", stop);
     return () => {
-      el.removeEventListener("mousedown", start);
-      el.removeEventListener("touchstart", start);
-      el.removeEventListener("mouseup", stop);
-      el.removeEventListener("touchend", stop);
+      currentNode.removeEventListener("mousedown", start);
+      currentNode.removeEventListener("touchstart", start);
+      currentNode.removeEventListener("mouseup", stop);
+      currentNode.removeEventListener("touchend", stop);
     };
-  });
-  return ref;
+  }, [currentNode, start, stop]);
+  return (node: HTMLElement | null) => {
+    if (node == null) {
+      setCurrentNode(null);
+      return;
+    }
+    setCurrentNode(node);
+  };
 };
