e24076250b32137ed84948c2420a38f2ad18a131
pccommen <pccommen@gmail.com>
2024-07-17 16:56:54 +0900

    feature: history를 담는 LogList구현

---
+++ src/LogList.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const LogList: React.FC = () => {
+    return (
+        <>
+            <div className="bg-slate-200 rounded-xl w-80">dsadas</div>
+        </>
+    )
+}
+
+export default LogList
