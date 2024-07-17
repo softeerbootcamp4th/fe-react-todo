22e55e6755f571369d59e0d84c048d82f1fa49f2
pccommen <pccommen@gmail.com>
2024-07-17 16:54:21 +0900

    fix: 진입점 App.tsx로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Header from './Header.tsx'
+import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Header />
+    <App />
   </React.StrictMode>,
 )
