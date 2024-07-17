33a5a386806e1dd828ba05281c2a940b97ae2583
pccommen <pccommen@gmail.com>
2024-07-17 18:35:00 +0900

    chore: tailwind.conig.js 테마 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,7 @@ export default {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {
-      flexGrow: {
-        2: '2',
-        3: '3',
-        4: '4',
-        5: '5',
-        6: '6',
-        7: '7',
-        8: '8',
-        9: '9'
-      }
-    },
-  },
+  theme: {},
   plugins: [],
 }
 
