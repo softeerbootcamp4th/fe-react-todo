46f68a261796b52249cf7be9c94472a9a5da5d34
pccommen <pccommen@gmail.com>
2024-07-17 18:37:25 +0900

    fix: header에 tailwind 적용

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <h1 className="px-20"> My Todo App</h1>
-    //<span className="font-bold text-6xl mt-16"> My Todo List</span> a8df159 (feature: Header 구현)
+    <span className="font-bold text-6xl mt-16"> My Todo List</span>
   )
 }
 
