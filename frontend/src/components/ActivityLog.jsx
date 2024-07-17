afb3f0ccf24073f904dbeea946744cc2d3562521
jw0097 <jwoo27@hanmail.net>
2024-07-17 17:22:08 +0900

    feat: todo 완료 기능 구현

--- frontend/src/components/ActivityLog.jsx
+++ frontend/src/components/ActivityLog.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 const ActivityLog = () => {
-  return <Container>ActivityLog</Container>;
+  return (
+    <Container>
+      <LogItem></LogItem>
+    </Container>
+  );
 };
 
 const Container = styled.div`
@@ -10,8 +14,10 @@ const Container = styled.div`
   position: absolute;
   right: 0;
   top: 0;
-  height: 100%;
+  height: 40rem;
   min-width: 20%;
 `;
+
+const LogItem = styled.div``;
 
 export default ActivityLog;
--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import { TodoStore } from "../Provider/todoContext";
 import { postToDoList } from "../api/todo";
+import { CLICK_THRESHOLD } from "../constants/magicNumber";
 
 const TodoList = () => {
   const { todoList, setTodoList } = useContext(TodoStore);
@@ -9,22 +10,28 @@ const TodoList = () => {
   return (
     <ListContainer>
       {todoList &&
-        todoList.map((todo, index) => {
-          return (
-            <TodoListItem
-              key={index}
-              index={index}
-              title={todo.title}
-              isLast={index === todoList.length - 1}
-            />
-          );
-        })}
+        todoList.map((todo, index) => (
+          <TodoListItem
+            key={index}
+            index={index}
+            title={todo.title}
+            isLast={index === todoList.length - 1}
+            isDone={todo.isDone}
+          />
+        ))}
     </ListContainer>
   );
 };
 
-const TodoListItem = ({ title, index, isLast }) => {
+const TodoListItem = ({ title, index, isLast, isDone }) => {
   const { todoList, setTodoList } = useContext(TodoStore);
+  const [isLongPressed, setIsLongPressed] = useState(false);
+  const timerRef = useRef(null);
+  const startTimeRef = useRef(0);
+
+  useEffect(() => {
+    console.log(todoList);
+  }, [todoList]);
 
   const onDeleteHandler = async (target) => {
     const newTodoList = await postToDoList(
@@ -32,9 +39,36 @@ const TodoListItem = ({ title, index, isLast }) => {
     );
     setTodoList(newTodoList);
   };
+
+  const onMouseDownHandler = () => {
+    startTimeRef.current = Date.now();
+
+    timerRef.current = setTimeout((target) => {
+      setIsLongPressed(true);
+    }, CLICK_THRESHOLD);
+  };
+
+  const onMouseUpHandler = (target) => {
+    if (Date.now() - startTimeRef.current < CLICK_THRESHOLD) {
+      clearTimeout(timerRef.current);
+      let front = todoList.slice(0, target);
+      let back = todoList.slice(target + 1);
+      let middle = todoList[target];
+      console.log(middle);
+      console.log("target", target);
+      middle.isDone = !middle.isDone;
+
+      setTodoList([...front, middle, ...back]);
+    }
+  };
+
   return (
-    <ItemContainer isLast={isLast}>
-      <ItemContent>{title}</ItemContent>
+    <ItemContainer
+      $isLast={isLast}
+      onMouseDown={() => onMouseDownHandler(index)}
+      onMouseUp={() => onMouseUpHandler(index)}
+    >
+      <ItemContent $isDone={isDone}>{title}</ItemContent>
       <ItemDeleteButton onClick={() => onDeleteHandler(index)}>
         삭제
       </ItemDeleteButton>
@@ -44,7 +78,9 @@ const TodoListItem = ({ title, index, isLast }) => {
 
 const ListContainer = styled.ul`
   width: 100%;
+  height: 20rem;
   padding: 0 1rem;
+  overflow: scroll;
 `;
 
 const ItemContainer = styled.div`
@@ -52,17 +88,20 @@ const ItemContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  align-items: center;
   margin-bottom: 0.5rem;
   padding-bottom: 0.5rem;
-  border-bottom: ${({ isLast }) => (isLast ? "none" : "1px solid gray")};
+  border-bottom: ${(props) => (props.$isLast ? "none" : "1px solid gray")};
 `;
 
 const ItemContent = styled.li`
   font-size: 16px;
+  text-decoration: ${(props) => (props.$isDone ? "line-through" : "none")};
 `;
 
 const ItemDeleteButton = styled.button`
   width: 3rem;
+  height: 100%;
   border: none;
   border-radius: 8px;
   background-color: #53a0e0;
