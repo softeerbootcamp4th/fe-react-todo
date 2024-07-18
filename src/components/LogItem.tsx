e67c35526ceb3d1ff057467babbfee945dab89ca
bluejoyq <bluejoy32@gmail.com>
2024-07-18 11:49:34 +0900

    feat: badge 추가

--- src/components/LogItem.tsx
+++ src/components/LogItem.tsx
@@ -1,10 +1,23 @@
 import { Log } from "../models/Log";
 import { formatDate } from "../utils/formatDate";
+import { Badge } from "./Badge";
 
 export const LogItem = ({ log }: { log: Log }) => {
+  const logTypeToBadgeType = () => {
+    switch (log.type) {
+      case "CREATE":
+        return "SUCCESS";
+      case "UPDATE":
+        return "INFO";
+      case "DELETE":
+        return "ERROR";
+      default:
+        return "SUCCESS";
+    }
+  };
   return (
     <div>
-      <div>{log.type}</div>
+      <Badge type={logTypeToBadgeType()}>{log.type}</Badge>
       <div>{log.message}</div>
       <div>{formatDate(log.createdAt)}</div>
     </div>
