c89cbaaa48c6a01c0433015c179608934361b91a
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-18 23:33:53 +0900

    add. Logs 화면 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import Header from "@/components/Header/Header"
-import Todo from '@/components/Todo/Todo'
-
+import Header from "@/components/Header/Header";
+import Todo from "@/components/Todo/Todo";
+import Log from "./components/Logs/Log";
 function App() {
-
   return (
-    <div className="flex flex-col items-center">
-      <Header />
-      <Todo />
+    <div className="flex w-[1000px] h-[500px] overflow-hidden">
+      <div className="flex flex-col items-center flex-grow">
+        <Header />
+        <Todo />
+      </div>
+      <div className="w-[400px] flex-shrink-0 flex justify-center">
+        <Log />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
