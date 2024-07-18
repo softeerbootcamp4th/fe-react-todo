4bdc7ac27c9c088fc5020e2087e041e77bdb8919
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-18 16:19:36 +0900

    feat. drag and drop list 구현

--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -1,18 +1,22 @@
-import { useState, useEffect, useRef } from "react";
-import Todo from "@/types/todoType";
-import { handleEdit } from "@/apis/fetch";
+import { useState, useRef } from "react";
+import { Todo } from "@/types/todoType";
+import { patchTodo } from "@/apis/todoList";
 import { useTodoContext } from "@/hooks/useTodoContext";
+import { patchTodoSplice, getToList } from "@/apis/todoList";
 
 function TodoList() {
-  const { isEdit, todoItemList, getTodoList, setTodoItemList, setIsEdit, handleDeleteTodoItem } = useTodoContext();
+  const { isEdit, todoItemList, setTodoItemList, setIsEdit, handleDeleteTodoItem } = useTodoContext();
   const timeRef = useRef<NodeJS.Timeout | null>(null);
   const [clickedId, setClickedId] = useState<number>(0);
+  const dragItem = useRef<number | undefined>();
+  const dragOverItem = useRef<number | undefined>();
+  const isDragging = useRef<boolean>(false);
   //const [currentTodo, setCurrentTodo] = useState<Partial<Todo>>({text: ""});
 
   const handleCompletedItem = (id: number) => {
     const updatedTodoList = todoItemList.map((todo) => {
       if (todo.id === id) {
-        handleEdit(id, { completed: !todo.completed });
+        patchTodo(todo.id, { completed: !todo.completed });
         return { ...todo, completed: !todo.completed };
       }
       return todo;
@@ -21,7 +25,31 @@ function TodoList() {
     setTodoItemList(updatedTodoList);
   };
 
+  const handleDragStart = (idx: number) => {
+    isDragging.current = true;
+    setIsEdit(false);
+    dragItem.current = idx;
+  };
+
+  const handleDragEnter = (idx: number) => {
+    if (!isEdit) dragOverItem.current = idx;
+  };
+
+  const handleDragEnd = async () => {
+    if (dragItem.current === undefined || dragOverItem.current === undefined) return;
+
+    patchTodoSplice({ targetIndex: dragItem.current, destinationIndex: dragOverItem.current })
+      .then(() => getToList())
+      .then((list) => {
+        setTodoItemList(list ?? []);
+      });
+
+    dragItem.current = undefined;
+    dragOverItem.current = undefined;
+  };
+
   const handleEditItem = (id: number, todo: Partial<Todo>) => {
+    if (isDragging.current) return;
     setClickedId(id);
     timeRef.current = setTimeout(() => {
       setIsEdit(true);
@@ -29,7 +57,6 @@ function TodoList() {
       /*
       setCurrentTodo({
         text: todo.text,
-        completed: todo.completed,
       }); */
     }, 2000);
   };
@@ -42,30 +69,30 @@ function TodoList() {
     }
   };
 
-  useEffect(() => {
-    getTodoList();
-  }, []);
-
   return (
     <div className="w-full">
       <ul>
-        {todoItemList.map((todo) => (
+        {todoItemList.map((todo, index) => (
           <li key={todo.id}>
             <div className="flex items-center justify-center">
               {isEdit && clickedId === todo.id ? (
-                <p className={`mr-5 flex-grow`} onClick={() => handleCompletedItem(todo.id)}>
-                  <input className="ml-[10px] border-2 border-solid w-[160px]" type="text" name="search input"></input>
-                </p>
+                <span className={`mr-5 flex-grow`} onClick={() => handleCompletedItem(todo.id)}>
+                  <input className="ml-[10px] border-2 border-solid w-[200px]" type="text" name="search input"></input>
+                </span>
               ) : (
-                <p
+                <span
                   className={`mr-5 flex-grow ${todo.completed ? "line-through text-gray-400" : ""}`}
                   onClick={() => handleCompletedItem(todo.id)}
                   onMouseDown={() => handleEditItem(todo.id, todo)}
                   onMouseUp={handleEditCancel}
                   onMouseLeave={handleEditCancel}
+                  draggable
+                  onDragStart={() => handleDragStart(index)}
+                  onDragEnter={() => handleDragEnter(index)}
+                  onDragEnd={handleDragEnd}
                 >
                   {todo.text}
-                </p>
+                </span>
               )}
               <button
                 className=" border-blue-300 border-1 border-solid bg-blue-400 text-white"
