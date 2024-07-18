c666fc7e6fa87497689b0aa359e654d8fc241fa0
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-18 23:36:44 +0900

    refactor. 불필요 주석 제거 & Todo UI 수정

--- src/components/Todo/TodoList.tsx
+++ src/components/Todo/TodoList.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState, useRef, ChangeEvent } from "react";
 import { Todo } from "@/types/todoType";
 import { patchTodo } from "@/apis/todoList";
 import { useTodoContext } from "@/hooks/useTodoContext";
@@ -7,11 +7,17 @@ import { patchTodoSplice, getTodoList } from "@/apis/todoList";
 function TodoList() {
   const { isEdit, todoItemList, setTodoItemList, setIsEdit, handleDeleteTodoItem } = useTodoContext();
   const timeRef = useRef<NodeJS.Timeout | null>(null);
-  const [clickedId, setClickedId] = useState<number>(0);
+  const [clickedId, setClickedId] = useState(0);
   const dragItem = useRef<number | undefined>();
   const dragOverItem = useRef<number | undefined>();
-  const isDragging = useRef<boolean>(false);
-  //const [currentTodo, setCurrentTodo] = useState<Partial<Todo>>({text: ""});
+  const isDragging = useRef(false);
+
+  const [editText, setEditText] = useState("");
+
+  const handleEditText = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setEditText(e.target.value);
+  };
 
   const handleCompletedItem = (id: number) => {
     const updatedTodoList = todoItemList.map((todo) => {
@@ -54,10 +60,6 @@ function TodoList() {
     timeRef.current = setTimeout(() => {
       setIsEdit(true);
       console.log(id, todo);
-      /*
-      setCurrentTodo({
-        text: todo.text,
-      }); */
     }, 2000);
   };
 
@@ -69,39 +71,64 @@ function TodoList() {
     }
   };
 
+  const patchEditText = (id: number) => {
+    patchTodo(id, { text: editText });
+    const updatedTodoList = todoItemList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, text: editText };
+      }
+      return todo;
+    });
+    setEditText("");
+    setTodoItemList(updatedTodoList);
+    setIsEdit(false);
+  };
   return (
     <div className="w-full">
       <ul>
         {todoItemList.map((todo, index) => (
-          <li key={todo.id}>
-            <div className="flex items-center justify-center">
+          <li key={todo.id} className="mb-4 p-4 border border-gray-200 rounded shadow-sm">
+            <div className="flex items-center justify-between">
               {isEdit && clickedId === todo.id ? (
-                <span className={`mr-5 flex-grow`} onClick={() => handleCompletedItem(todo.id)}>
-                  <input className="ml-[10px] border-2 border-solid w-[200px]" type="text" name="search input"></input>
-                </span>
+                <>
+                  <span className="mr-5 flex-grow" onClick={() => handleCompletedItem(todo.id)}>
+                    <input
+                      className="ml-2 border-2 border-solid w-48"
+                      type="text"
+                      name="text edit"
+                      onChange={handleEditText}
+                    />
+                  </span>
+                  <button
+                    className="border-1 border-solid bg-blue-300 text-white py-1 px-3 rounded"
+                    onClick={() => patchEditText(todo.id)}
+                  >
+                    수정
+                  </button>
+                </>
               ) : (
-                <span
-                  className={`mr-5 flex-grow ${todo.completed ? "line-through text-gray-400" : ""}`}
-                  onClick={() => handleCompletedItem(todo.id)}
-                  onMouseDown={() => handleEditItem(todo.id, todo)}
-                  onMouseUp={handleEditCancel}
-                  onMouseLeave={handleEditCancel}
-                  draggable
-                  onDragStart={() => handleDragStart(index)}
-                  onDragEnter={() => handleDragEnter(index)}
-                  onDragEnd={handleDragEnd}
-                >
-                  {todo.text}
-                </span>
+                <>
+                  <span
+                    className={`mr-5 flex-grow ${todo.completed ? "line-through text-gray-400" : ""}`}
+                    onClick={() => handleCompletedItem(todo.id)}
+                    onMouseDown={() => handleEditItem(todo.id, todo)}
+                    onMouseUp={handleEditCancel}
+                    draggable
+                    onDragStart={() => handleDragStart(index)}
+                    onDragEnter={() => handleDragEnter(index)}
+                    onDragEnd={handleDragEnd}
+                  >
+                    {todo.text}
+                  </span>
+                  <button
+                    className="border-1 border-solid bg-blue-300 text-white py-1 px-3 rounded"
+                    onClick={() => handleDeleteTodoItem(todo.id)}
+                  >
+                    삭제
+                  </button>
+                </>
               )}
-              <button
-                className=" border-blue-300 border-1 border-solid bg-blue-400 text-white"
-                onClick={() => handleDeleteTodoItem(todo.id)}
-              >
-                삭제
-              </button>
             </div>
-            <div className="h-[0.5px] bg-green-300"></div>
           </li>
         ))}
       </ul>
