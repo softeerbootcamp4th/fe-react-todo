973ca61b7d875a00922d55793ccc4d8050688941
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-18 16:24:31 +0900

    feat. patch,post interface 정의

--- src/types/todoType.ts
+++ src/types/todoType.ts
@@ -1,5 +1,20 @@
-export default interface Todo {
-    id: number;
-    text: string;
-    completed: boolean;
+export interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+export interface PostTodoRequestBody {
+  text: string;
+  completed: boolean;
+}
+
+export interface PatchTodoSpliceRequestBody {
+  targetIndex: number;
+  destinationIndex: number;
+}
+
+export interface PatchTodoRequestBody {
+  text?: string;
+  completed?: boolean;
 }
