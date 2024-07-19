bf4c53c321a9afabbd3c5823da1346ca8192d7ce
pccommen <pccommen@gmail.com>
2024-07-19 11:33:43 +0900

    feature: LogList 구현

--- src/LogList.tsx
+++ src/LogList.tsx
@@ -1,11 +1,22 @@
 import React from "react"
+import LogElementProps, { LogListElement } from "./LogListElement"
 
-const LogList: React.FC = () => {
+interface LogListProps {
+    logList: LogElementProps[]
+}
+
+const LogList: React.FC<LogListProps> = ({ logList }: LogListProps) => {
+
+    const content = logList.map((log) => (
+        <LogListElement todo={log.todo} status={log.status} />
+    ))
+
     return (
-        <>
-            <div className="bg-slate-200 rounded-xl w-80">Log</div>
-        </>
+        <div className="bg-slate-200 rounded-xl w-80 flex flex-col items-center w-full overflow-y-scroll">
+            {content}
+        </div>
     )
 }
 
-export default LogList
+export { LogList }
+export default LogListProps
