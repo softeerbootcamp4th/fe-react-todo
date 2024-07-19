5959ec634c5fe6bfdebb2110bb19ce9f3910cda7
fairwheel3027148 <popdarnq5@gmail.com>
2024-07-19 12:06:03 +0900

    fix. 절대경로

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import Header from "@/components/Header/Header";
 import Todo from "@/components/Todo/Todo";
-import LogHeader from "./components/Logs/LogHeader";
-import LogItem from "./components/Logs/Logitem";
+import LogHeader from "@/components/LogList/LogHeader";
+import LogItem from "@/components/LogList/Logitem";
 
 function App() {
   return (
     <div className="flex w-[1000px] h-[500px] overflow-hidden">
-      <div className="flex flex-col items-center flex-grow">
+      <div className="flex flex-col items-center flex-grow shadow-md">
         <Header />
         <Todo />
       </div>
-      <div className="w-[400px] flex-shrink-0 flex flex-col">
+      <div className="w-[400px] flex-shrink-0 flex flex-col shadow-md px-4">
         <LogHeader />
         <LogItem />
       </div>
